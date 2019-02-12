73bf8bd59456680ae90938d946642afc6cf55195
高山 雄一郎 <takayamayuuichirou@kouzan-yuuichirou.local>
2019-02-13 08:43:07 +0900

    rails 05

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true,
+    length: { maximum: 15}
+
+  validates :email, presence: true,
+    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 
   has_secure_password
+  validates :password, :password_confirmation, format: {with: /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/i},
+    length:{ minimum:8, maximum:32}
 end
